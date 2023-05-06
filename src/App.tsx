622d6579ded1c711a6df324ea990ea6e067182e1
xxKeefer <xxkeefer.code@gmail.com>
2023-05-06 13:59:13 +1000

    feat: :sparkles: final code solution for talk

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Container } from "./components";
 import { useUserActions } from "./hooks/useUserActions";
 import { UserProfile } from "./types";
-import { BusinessCard } from "./components/ExampleOne/BusinessCard";
+// import { BusinessCard } from "./components/ExampleOne/BusinessCard";
 // import { BusinessCard } from "./components/ExampleTwo/BusinessCard";
-// import { default as BusinessCard } from "./components/FinalExample/BusinessCard";
+import { default as BusinessCard } from "./components/FinalExample/BusinessCard";
+
+const { Banner, Location, Name, Pronouns, Socials, Tagline } = BusinessCard;
 
 const user: UserProfile = {
   image: "/wizard.jpeg",
@@ -24,17 +26,26 @@ function App() {
   const { onConnect, onMessage } = useUserActions(user);
   return (
     <Container>
-      <BusinessCard
-        bgImage={user.bgImage}
-        image={user.image}
-        location={user.location}
-        name={user.name}
-        pronouns={user.pronouns}
-        tagline={user.tagline}
-        socials={user.socials}
-        onConnect={onConnect}
-        onMessage={onMessage}
-      />
+      <BusinessCard user={user}>
+        <Banner />
+        <Socials />
+        <div className="card-body p-4">
+          <h2>
+            <Name />
+            <Pronouns />
+          </h2>
+          <Tagline />
+          <Location />
+          <div className="card-actions flex-nowrap pt-4">
+            <button className="btn-primary btn grow" onClick={onConnect}>
+              Connect
+            </button>
+            <button className="btn-secondary  btn grow" onClick={onMessage}>
+              Message
+            </button>
+          </div>
+        </div>
+      </BusinessCard>
     </Container>
   );
 }
